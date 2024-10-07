9a7cc61393b96820a80559d9b186e3aaa94f3432
Sumit0952 <sumitkumar.200259@gmail.com>
2024-10-07 15:41:52 +0530

    Drawer Nav

--- App.jsx
+++ App.jsx
@@ -1,20 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native'
+import 'react-native-gesture-handler';
+
+import { StyleSheet, View } from 'react-native'
 import React from 'react'
 import Header from './src/components/Header'
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import Settings from './src/screens/Settings';
+import Favorites from './src/screens/Favorites';
+import { enableScreens } from 'react-native-screens';
 
+const Drawer = createDrawerNavigator();
+enableScreens();
 const App = () => {
-  return (
-    <View style = {styles.container}>
-      <Header/>
-    </View>
-  )
+  return <NavigationContainer>
+      <View style={styles.container}>
+        {/* Add Header here */}
+        <Header />
+        <Drawer.Navigator>
+          <Drawer.Screen name="Settings" component={Settings} />
+          <Drawer.Screen name="Favorites" component={Favorites} />
+        </Drawer.Navigator>
+      </View>
+    </NavigationContainer>
+  
 }
 
 export default App
 
 const styles = StyleSheet.create({
   container:{
-    flex:1,
-    
+    flex: 1,
   }
 })
